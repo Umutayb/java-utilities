33c21015d869679df2d3e27e7b33ed5eef6feb85
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-06-04 15:12:58 +0200

    Reverted

--- src/main/java/utils/Printer.java
+++ src/main/java/utils/Printer.java
@@ -2,32 +2,19 @@ package utils;
 
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
-import java.io.File;
 import java.io.FileReader;
 import java.util.Properties;
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 import resources.Colors;
 
 public class Printer extends Colors {
 
     private final Log log;
-    private final Logger logger = Logger.getGlobal();
     private final Properties properties = new Properties();
 
     public <T> Printer(Class<T> className){
         log = LogFactory.getLog(className);
-        SimpleFormatter formatter = new SimpleFormatter();
-        try {
-            properties.load(new FileReader("src/test/resources/test.properties"));
-            FileUtilities fileUtils = new FileUtilities();
-            fileUtils.createIfAbsent("src/test/resources/files/Record.log");
-            FileHandler file = new FileHandler("target/Record.log");
-            file.setFormatter(formatter);
-            logger.addHandler(file);
-        }
+        try {properties.load(new FileReader("src/test/resources/test.properties"));}
         catch (IOException e) {e.printStackTrace();}
     }
 
@@ -43,8 +30,8 @@ public class Printer extends Colors {
 
     public void report(String color, Object text){
         if (Boolean.parseBoolean(properties.getProperty("enableLogging")))
-            logger.info((String) text);
-        else
             log.info(color + text + RESET);
+        else
+            System.out.println((String) text);
     }
 }
