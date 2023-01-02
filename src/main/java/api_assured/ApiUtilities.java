899982b2574f2208cc5dd3354ca5ebb8811627e4
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2023-01-02 14:39:14 +0100

    Added dynamic logger

--- src/main/java/api_assured/ApiUtilities.java
+++ src/main/java/api_assured/ApiUtilities.java
@@ -1,13 +1,15 @@
 package api_assured;
 
-import utils.FileUtilities;
-import utils.Printer;
+import utils.*;
 
 import java.util.Properties;
 
 public abstract class ApiUtilities extends Caller {
     public static Properties properties;
-    Printer log = new Printer(ApiUtilities.class);
+    public StringUtilities strUtils = new StringUtilities();
+    public ObjectUtilities objectUtils = new ObjectUtilities();
+    public NumericUtilities numUtils = new NumericUtilities();
+    public Printer log = new Printer(this.getClass());
 
     public ApiUtilities(){properties = FileUtilities.properties;}
 }
