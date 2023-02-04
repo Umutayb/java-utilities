a9dc7246c2c35ffaaeb1787fa2f1c81c08f6a187
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2023-02-04 21:56:58 +0100

    Update TerminalUtilities.java

--- src/main/java/utils/TerminalUtilities.java
+++ src/main/java/utils/TerminalUtilities.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.io.File;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,25 +9,25 @@ public class TerminalUtilities {
 
     private final Printer log = new Printer(TerminalUtilities.class);
 
-    public void runTerminalCommand(String command, String logText){
-        try {
-            String path = System.getProperty("user.dir");
-            ProcessBuilder builder = new ProcessBuilder(
-                    "Terminal.app", "usr/", "cd \""+path+"\" && "+command);
-            builder.redirectErrorStream(true);
-            Process process = builder.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while (true){
-                line = reader.readLine();
-                if (line.contains(logText)){
-                    Thread.sleep(5000);
-                    log.new Success(line);
-                    break;
-                }
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void runCommand(String command) throws IOException, InterruptedException {
+
+        log.new Info("Running command: " + command);
+
+        String homeDirectory = System.getProperty("user.home");
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+        if (isWindows) processBuilder.command("cmd.exe", "/c", command);
+        else processBuilder.command("sh", "-c", command);
+
+        processBuilder.directory(new File("."));
+        processBuilder.redirectErrorStream(true);
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        //Process process = processBuilder.inheritIO().start();
+        Process process = processBuilder.start();
+        process.waitFor();
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//		reader.lines().forEach(System.out::println);
+
     }
 }
