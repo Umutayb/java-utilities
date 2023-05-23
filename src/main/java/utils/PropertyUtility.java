6efe8eda81e337046dadab32e278675f381f7ec1
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2023-05-23 02:33:05 +0200

    Updated PropertyUtility!
    PropertyUtility.java now supports loading properties from just the property file name!

--- src/main/java/utils/PropertyUtility.java
+++ src/main/java/utils/PropertyUtility.java
@@ -1,9 +1,8 @@
 package utils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -11,12 +10,37 @@ import java.util.Properties;
  */
 public class PropertyUtility {
 
+    private static final Printer log = new Printer(PropertyUtility.class);
+
     /**
      * The Properties object containing the loaded properties.
      */
     public static final Properties properties = new Properties();
 
-    private static FileUtilities fileUtilities = new FileUtilities();
+    protected static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+    /**
+     * No args constructor.
+     */
+    public PropertyUtility(){}
+
+    /**
+     Initializes a PropertyUtility object by loading properties from the specified property file.
+     The property file should be present in the classpath.
+     @param propertyFileName The name of the property file to load.
+     */
+    public PropertyUtility(String propertyFileName){
+            try {
+                InputStream inputStream = getClass().getClassLoader()
+                        .getResourceAsStream(propertyFileName);
+                properties.load(inputStream);
+            }
+            catch (Exception ignored) {
+                log.warning(propertyFileName + " file not found!");
+            }
+    }
 
     /**
      * Loads the properties from the specified file path.
@@ -26,21 +50,9 @@ public class PropertyUtility {
      * @throws RuntimeException if there is an error while loading the properties
      */
     public static void loadProperties(String path){
-        try {
-            properties.load(new FileReader(path));
-        } catch (FileNotFoundException notFoundException){
-            try {
-                fileUtilities.createIfAbsent(path);
-                properties.load(new FileReader(path));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
+        try {properties.load(new FileReader(path));}
+        catch (IOException ioException){log.error(ioException.getMessage(), ioException);}
     }
 
-    protected static String getProperty(String key){
-        return properties.getProperty(key);
-    }
+    public Properties getProperties(){return properties;}
 }
