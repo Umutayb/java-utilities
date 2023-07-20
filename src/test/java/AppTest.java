d91b4a67eb82ff9b388d89bd36c352d5e1132ba5
egecan <benegecansen@gmail.com>
2023-07-20 20:13:39 +0300

    AppTest update
    - Added unit test for the stringToObject feature

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,4 +1,6 @@
+import models.Pet;
 import org.junit.Test;
+import utils.MappingUtilities;
 import utils.Printer;
 
 public class AppTest {
@@ -8,6 +10,12 @@ public class AppTest {
     @Test
     public void dataGeneratorPetTest() {
         printer.info("Test!");
+    }
+
+    @Test
+    public void stringToObjectTest() {
+        Pet pet = MappingUtilities.stringToObject("{\"id\" : null, \"category\" : {\"id\" : null, \"name\" : \"Cats\"},\"name\" : \"Whiskers\", \"photoUrls\" : [ \"https://example.com/cat.jpg\" ],\"tags\" : [ {\"id\" : 123456789, \"name\" : \"Furry\"}, {\"id\" : 987654321, \"name\" : \"Playful\"} ],\"status\" : \"Available\"}", Pet.class);
+        System.out.println(pet);
     }
 
 }
