7c71c54b7e978b1709e9bac70d4032f2797d4e18
Umut Ay <umutaybora@gmail.com>
2025-02-05 15:05:24 +0100

    Update ArrayUtilities.java

--- src/main/java/utils/arrays/ArrayUtilities.java
+++ src/main/java/utils/arrays/ArrayUtilities.java
@@ -1,5 +1,6 @@
 package utils.arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -39,6 +40,42 @@ public class ArrayUtilities {
      */
     public static <ItemType> boolean isLastMemberOf(List<ItemType> items, ItemType member){
         return items.indexOf(member) == (items.size() - 1);
+    }
+
+    /**
+     * Splits a given list into partitions and returns a specific partition based on the partition index.
+     *
+     * This method uses stream filtering to find and return the elements that belong to the requested partition.
+     * The partition index is 0-based.
+     *
+     * @param <T> The type of elements in the list.
+     * @param originalList The original list to be partitioned.
+     * @param partitionSize The size of each partition.
+     * @param partitionIndex The index of the partition to retrieve (0-based index).
+     * @return A list containing the elements from the specified partition.
+     * @throws IndexOutOfBoundsException If the partitionIndex is out of bounds (negative or too large).
+     */
+    public static <T> List<T> getListPartition(List<T> originalList, int partitionSize, int partitionIndex){
+        int partitionBottomLimit = partitionSize * partitionIndex;
+        return originalList.stream().filter(item -> {
+            int itemIndex = originalList.indexOf(item);
+            return partitionBottomLimit <= itemIndex && itemIndex < partitionSize + partitionBottomLimit;
+        }).toList();
+    }
+
+    /**
+     * Calculates the total number of partitions needed to divide a list of a given size into partitions
+     * of the specified size.
+     *
+     * If there are leftover elements after dividing the list, an extra partition is counted.
+     *
+     * @param listSize The total number of elements in the list.
+     * @param partitionSize The size of each partition.
+     * @return The number of partitions required to divide the list.
+     */
+    public static int getPartitionCount(int listSize, int partitionSize){
+        int partitionCount = listSize / partitionSize;
+        return (partitionCount * partitionSize > listSize ? partitionCount + 1 : partitionCount);
     }
 }
 
